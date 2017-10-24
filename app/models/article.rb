0eae1fa9ecdfb7d293786bf61f30a25b89adcff3
Samitha Padukkage Don <samitha.312@gmail.com>
2017-10-24 13:19:01 +1100

    added a few gems and image handling

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,8 @@ class Article < ApplicationRecord
   has_many :comments
   has_many :taggings
   has_many :tags, through: :taggings
+  has_attached_file :image, styles: { large: "800x800>", medium: "500x500>", thumb: "100x100>" }
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
   def tag_list
     self.tags.collect do |tag|
